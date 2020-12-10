bd08b02ea0e59b6101420c55f2e59c0d57a1b6d0
Michael Pearson <michaelpearsun@gmail.com>
2020-12-10 12:49:45 -0600

    Intital Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+import Directory from './components/DirectoryComponent';
+import { CAMPSITES } from './shared/campsites'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       stateDataCampsites: CAMPSITES
+    }
+  }
+  
+
+    render() {
+        return (
+            <div className="App">
+                <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">NuCamp</NavbarBrand>
+                </div>
+                </Navbar>
+                <Directory propsForCampsitesFromApp={this.state.stateDataCampsites}/>
+            </div>
+        );
+    }
+}
+
+export default App;
